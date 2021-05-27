1767d8f3db28dc3056c7a97e971b21c18b00685e
Margo-V <flautorfei1929@gmail.com>
2021-05-27 12:36:05 +0400

    Add project files.

---
+++ Games/main.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(200, 200), "15th");
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+    }
+    window.display();
+}
+
